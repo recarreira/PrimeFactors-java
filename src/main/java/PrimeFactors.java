03246e86074b8fd04283ccb9483caed7c5606f0d
Renata Carreira <re.carreira@gmail.com>
2015-01-08 17:18:59 -0200

    PrimeFactor.generate: function working

--- src/main/java/PrimeFactors.java
+++ src/main/java/PrimeFactors.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
+
 /**
  * Created by rcarreira on 1/7/15.
  */
 public class PrimeFactors {
     public boolean isPrime(int number){
-        if(number == 2){
-            return true;
+
+        if(number == 1){
+            return false;
+        }else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0){
+                    return false;
+                }
+            }
         }
-        return false;
+        return true;
+    }
+
+    public ArrayList<Integer> generate(int number) {
+        ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0){
+                if (isPrime(i)){
+                    primeFactors.add(i);
+                }
+            }
+        }
+
+        return primeFactors;
     }
 }
--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -1,6 +1,10 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class PrimeFactorsTest {
@@ -21,4 +25,37 @@ public class PrimeFactorsTest {
     public void isPrimeShouldReturnTrueForNumber2(){
         assertEquals(true, examplePrimeFactor.isPrime(2));
     }
+
+    @Test
+    public void isPrimeShouldReturnTrueForNumber3(){
+        assertEquals(true, examplePrimeFactor.isPrime(3));
+    }
+
+    @Test
+    public void isPrimeShouldReturnFalseForNumber4(){
+        assertEquals(false, examplePrimeFactor.isPrime(4));
+    }
+
+    @Test
+    public void isPrimeShouldReturnTrueForNumber5(){
+        assertEquals(true, examplePrimeFactor.isPrime(5));
+    }
+
+    @Test
+    public void isPrimeShouldReturnFalseForNumber9(){
+        assertEquals(false, examplePrimeFactor.isPrime(9));
+    }
+
+    @Test
+    public void generateShouldReturnNumbers2and3and5forNumber30(){
+        ArrayList <Integer> primeFactors = new ArrayList<Integer>(Arrays.asList(2, 3, 5));
+        assertEquals(primeFactors, examplePrimeFactor.generate(30));
+    }
+
+    @Test
+    public void generateShouldReturnAnEmptyListForNumberOne(){
+        ArrayList<Integer> primeFactors = new ArrayList<Integer>();
+        assertEquals(primeFactors, examplePrimeFactor.generate(1));
+    }
+
 }
